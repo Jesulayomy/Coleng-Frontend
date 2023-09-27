7f61617ff49e011854520038900ded12ef4748ed
DevEmmy <eolaosebikan60@gmail.com>
2023-09-27 02:33:30 +0100

    updated information page

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,16 +6,31 @@ const Information = () => {
   return (
     <>
       <Nav />
-      <div classname="flex gap-10 px-xPadding items-center py-20 sm:flex-col sm:p-sPadding">
-        <div>
-          <h2>Get know us</h2>
+
+      {/* <div classname="flex flex-row gap-10 px-xPadding py-20 sm:flex-col sm:p-sPadding">
+        <div className='w-1/2'>
+          <h2 className=''>Get know us</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quo hic, culpa beatae minus iste vitae odit vero illum, inventore sint itaque aperiam illo eos obcaecati facilis. Optio, voluptas rerum?</p>
         </div>
-        <div><img src='/smiling.png' /></div>
+
+        <div>
+          <img src='/smiling.png' className='w-1/2 h-auto'/>
+          </div>
+      </div> */}
+
+    <div className="flex gap-10 flex-row p-xPadding items-center">
+      <div className='flex gap-3 flex-col'>
+        <h2 className='text-[2.5em] font-[700]' >Get Informed.</h2>
+        <p className='text-gray-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quo hic, culpa beatae minus iste vitae odit vero illum, inventore sint itaque aperiam illo eos obcaecati facilis. Optio, voluptas rerum?</p>
+        <button className='bg-primary1 text-white px-5 py-2 rounded-md w-fit'>
+          Continue
+        </button>
       </div>
+      
       <div>
-        <h2>Updates</h2>
+        <img src="./smiling.png" alt="" />
       </div>
+    </div>
       <Footer />
     </>
   )
