d9b5c4e5caca652271f4c0eb33b210a767f425d3
Ivylegend <victoryicha2@gmail.com>
2023-10-05 07:19:57 +0100

    edits on the information page

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,6 +1,6 @@
-import Footer from '@/atomic_components/Footer';
-import Nav from '@/atomic_components/Nav';
-import React from 'react';
+import Footer from "@/atomic_components/Footer";
+import Nav from "@/atomic_components/Nav";
+import React from "react";
 
 const eventData = [
   {
@@ -24,19 +24,24 @@ const eventData = [
     month: "FEB",
     year: "2023",
   },
-]
+];
 
 const Information = () => {
   return (
     <>
       <Nav active={3} />
-      <div className="flex gap-10 flex-row p-xPadding items-center">
-        <div className='flex gap-3 flex-col'>
-          <h2 className='text-[2.5em] font-[700]' >Get Informed.</h2>
-          <p className='text-gray-700'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quo hic, culpa beatae minus iste vitae odit vero illum, inventore sint itaque aperiam illo eos obcaecati facilis. Optio, voluptas rerum?</p>
-          <button className='bg-primary1 text-white px-5 py-2 rounded-md w-fit'>
+      <div className="flex gap-10 flex-row p-xPadding items-center sm:flex-col sm:justify-center">
+        <div className="flex gap-3 flex-col">
+          <h2 className="text-[2.5em] font-[700]">Stay Informed...</h2>
+          <p className="text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+            quo hic, culpa beatae minus iste vitae odit vero illum, inventore
+            sint itaque aperiam illo eos obcaecati facilis. Optio, voluptas
+            rerum?
+          </p>
+          <button className="bg-primary1 text-white px-5 py-2 rounded-md w-fit">
             Continue
-        </button>
+          </button>
         </div>
 
         <div>
@@ -45,33 +50,46 @@ const Information = () => {
       </div>
 
       {/* UPDATES */}
-      <h2 className="font-[500] text-[2rem] text-center">Updates</h2>
-      <p className="text-center">Stay updated with the latest information about our college</p>
-      <div></div>
+      <div className="p-xPadding">
+        <h2 className="font-[500] text-[2rem] text-center">Updates</h2>
+        <p className="text-center">
+          Stay updated with the latest information about our college
+        </p>
+        <div></div>
+      </div>
 
       {/* UPCOMING EVENTS */}
       <div className="p-xPadding">
         <h2 className="font-[500] text-[2rem] text-center">Upcoming Events</h2>
         {eventData.map((event) => {
-          return <>
-            <div className="flex flex-row items-center justify-center gap-[5rem] py-[2rem] border-b-[1px]">
-              <div className="bg-black py-[1rem] px-[2rem] flex flex-col items-center">
-                <span className="text-[2rem] block">{event.day}</span>
-                <br /> FEB
-             <br /> 2023
-            </div>
-              <div>
-                <h3 className="text-gray mb-[1rem]">{event.title}</h3>
-                <p className="text-[0.8rem]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. At natus saepe odio porro non fugit?</p>
+          return (
+            <>
+              <div className="flex flex-row items-center justify-center gap-[5rem] py-[2rem] border-b-[1px] sm:flex-col sm:items-start sm:gap-5">
+                <div className="bg-black text-white py-[1rem] px-[2rem] flex flex-col items-center sm:flex-row sm:w-[100%]">
+                  <span className="text-[2rem] block">{event.day}</span>
+                  <br /> FEB
+                  <br /> 2023
+                </div>
+                <div>
+                  <h3 className="text-gray mb-[1rem] font-semibold">
+                    {event.title}
+                  </h3>
+                  <p className="text-[0.8rem]">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. At
+                    natus saepe odio porro non fugit?
+                  </p>
+                </div>
+                <button className="btn px-5 py-2 rounded-md border-[2px]">
+                  Learn More
+                </button>
               </div>
-              <button className="btn px-5 py-2 rounded-md border-[2px]">Learn More</button>
-            </div>
-          </>
+            </>
+          );
         })}
       </div>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Information
+export default Information;
