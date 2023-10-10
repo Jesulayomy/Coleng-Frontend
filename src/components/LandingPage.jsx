3f0c8db5b92e8b33b94f46c47f53e4d039788a30
Ivylegend <victoryicha2@gmail.com>
2023-10-10 13:30:01 +0100

    events

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,47 +1,27 @@
-<<<<<<< HEAD
 "use client";
 import Banner from "@/atomic_components/Banner";
 import Banner2 from "@/atomic_components/Banner2";
 import Footer from "@/atomic_components/Footer";
 import ImageAndText from "@/atomic_components/ImageAndText";
 import Nav from "@/atomic_components/Nav";
-import Event from "@/atomic_components/Event";
 import Team from "@/atomic_components/Team";
+import Event from "@/atomic_components/Event";
 import React from "react";
-=======
-"use client"
-import Banner from '@/atomic_components/Banner'
-import Banner2 from '@/atomic_components/Banner2'
-import Footer from '@/atomic_components/Footer'
-import ImageAndText from '@/atomic_components/ImageAndText'
-import Nav from '@/atomic_components/Nav'
-import Team from '@/atomic_components/Team'
-import React from 'react'
-import Objectives from './Objectives'
-import NewsLetter from '@/atomic_components/NewsLetter'
->>>>>>> fbb3f4152b63025c362da92d3fce2f47b3787f5b
+import Objectives from "./Objectives";
+import NewsLetter from "@/atomic_components/NewsLetter";
 
 const LandingPage = () => {
   return (
     <>
-<<<<<<< HEAD
       <Nav />
       <Banner />
       <Banner2 />
-      <ImageAndText />
+      <ImageAndText shorten={true} />
+      <Objectives />
       <Event />
       <Team />
+      <NewsLetter />
       <Footer />
-=======
-        <Nav />
-        <Banner />
-        <Banner2 />
-        <ImageAndText shorten={true} />
-        <Objectives />
-        <Team />
-        <NewsLetter />
-        <Footer />
->>>>>>> fbb3f4152b63025c362da92d3fce2f47b3787f5b
     </>
   );
 };
