390aeb9931e98d735bb8591a711cb133729ab29d
DevEmmy <eolaosebikan60@gmail.com>
2024-01-15 11:57:19 +0100

    updated speech

--- src/atomic_components/ImageAndText.jsx
+++ src/atomic_components/ImageAndText.jsx
@@ -9,28 +9,15 @@ const ImageAndText = ({shorten = false}) => {
 <div className="sm:px-2">
         <h2 className="text-[2em] font-[700]">President's speech.</h2>
         <p className="py-2 text-gray-700">
-          The Nigerian Universities Engineering Students' Association (NUESA),
-          FUNAAB Chapter, is a leading student organization that promotes
-          technological innovation and excellence among its members for the
-          benefit of society
+        The Nigerian Universities Engineering Students' Association (NUESA), FUNAAB Chapter, is a leading student organization that promotes technological innovation and excellence among its members for the benefit of society
         </p>
 
         <p className="py-2 text-gray-700">
-          I am honored to be the current president of NUESA, FUNAAB Chapter,
-          which has about 1,500 members from five departments in the College of
-          Engineering. With the help of my dedicated Executive team(Team
-          Synergy), Dean of college of engineering, Staff Adviser and
-          Patron/Matron, we work to ensure that every NUESA member has a smooth
-          and successful journey through the college.{" "}
+        I am honored to be the current president of NUESA, FUNAAB Chapter, which has over 1,500 members from five departments in the College of Engineering. With the help of my dedicated Executive team(Team Versatility), Dean of college of engineering, Staff Adviser and Patron/Matron, we work to ensure that every NUESA member has a smooth and successful journey through the college.
         </p>
 
         <p className="py-2 text-gray-700">
-          As a student branch, our goal is to uphold the organization's
-          mission of technological excellence and professional development for
-          students. We aim to provide our members with opportunities to enhance
-          their skills both academically and professionally. We strive to be
-          successful in helping to develop outstanding engineers who will make
-          positive impacts throughout the world.{" "}
+        As a student branch, our goal is to uphold the organization's mission of technological excellence and professional development for students. We aim to provide our members with opportunities to enhance their skills both academically and professionally. We strive to be successful in helping to develop outstanding engineers who will make positive impacts throughout the world.{" "}
         </p>
 
         {
@@ -45,23 +32,13 @@ const ImageAndText = ({shorten = false}) => {
 
           <>
           <p className="py-2 text-gray-700">
-          We have undertaken a number of initiatives to support the academic and
-          technical growth of our students. These include: Starting a Tech
-          community, Organizing weekly academic tutorials, Fresh students
-          orientation program, NUESA fresher's cup, Informative and interactive
-          webinars, Health, safety, and environment training, Project management
-          training and certification, Career fair, Intra-collegiate debate
-          competition, 7th Annual Engineering conference, NUESA Week '23, NUESA
-          Olympics, Launching a website and so on.
+          We have undertaken a number of initiatives to support the academic and technical growth of our students. These include: Starting a Tech community, having a tech Lounge, Organizing weekly academic tutorials, Fresh students orientation program, Intellectual competitions, NUESA fresher's cup coming up, along side the following ones like Informative and interactive webinars, Health, and safety, and environment training, Project management training and certification, Career fair, Intra-collegiate debate competition, 8th Annual Engineering conference, NUESA Week '24, NUESA Olympics, Treasure Hunt, Launching a website and so on.
         </p>
         <p className="py-2 text-gray-700">
-          We also plan to embark on a physical building project(Tech lounge),
-          and to host more webinars. We are highly dedicated to helping our
-          students achieve academic and technical excellence.
+        We also plan to embark on a physical building project(Relaxation Centre), and to host more webinars. We are highly dedicated to helping our students achieve academic and technical excellence.
         </p>
         <p className="py-2 text-gray-700">
-          I hereby use this opportunity to welcome you to the best college in
-          the Federal University of Agriculture Abeokuta.
+          I hereby use this opportunity to welcome you to the best college in the Federal University of Agriculture Abeokuta.
         </p>
           </>
         }
